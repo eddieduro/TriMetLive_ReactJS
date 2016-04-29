c577a18cc6062da32023d1711d933a333cc4d2c7
eddie duro <eddieduro@eddies-MacBook-Pro.local>
2016-04-29 09:29:00 -0700

    display navigation on app page

---
+++ index.ios.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+'use strict';
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS
+} from 'react-native';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello World!
+        </Text>
+      </View>
+    );
+  }
+}
+
+class TriMetLiveApp extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.wrapper}
+        initialRoute={{
+          title: 'TriMetLive',
+          component: HelloWorld,
+        }}/>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  wrapper: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('TriMetLiveApp', () => TriMetLiveApp);
