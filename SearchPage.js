c804d57498084008c49a17170c2b1d637c1533c6
eddie duro <eddieduro@eddies-MacBook-Pro.local>
2016-04-29 09:54:18 -0700

    add styling to UI components - input for and buttons

--- SearchPage.js
+++ SearchPage.js
@@ -22,6 +22,19 @@ class SearchPage extends Component {
 				<Text style={styles.description}>
 					Search by zip-code, address, or near your location
 				</Text>
+				<View style={styles.flowRight}>
+					<TextInput
+						style={styles.searchInput}
+						placeholder="Search via street address or zipcode"/>
+					<TouchableHighlight style={styles.button}
+						underlayColor='#99d9f4'>
+						<Text style={styles.buttonText}>Go</Text>
+					</TouchableHighlight>
+				</View>
+				<TouchableHighlight style={styles.button}
+					underlayColor='#99d9f4'>
+					<Text style={styles.buttonText}>Location</Text>
+				</TouchableHighlight>
 			</View>
 		);
 	}
@@ -38,6 +51,39 @@ const styles = StyleSheet.create({
 		padding: 30,
 		marginTop: 65,
 		alignItems: 'center',
+	},
+	flowRight: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		alignSelf: 'stretch',
+	},
+	buttonText: {
+		fontSize: 18,
+		color: 'white',
+		alignSelf: 'center',
+	},
+	button: {
+		height: 36,
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor: '#48BBEC',
+		borderColor: '#48BBEC',
+		borderWidth: 1,
+		borderRadius: 8,
+		marginBottom: 10,
+		alignSelf: 'stretch',
+		justifyContent: 'center',
+	},
+	searchInput: {
+		height: 36,
+		padding: 4,
+		marginRight: 5,
+		flex: 4,
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: '#48BBEC',
+		borderRadius: 8,
+		color: '#48BBEC',
 	},
 });
 
